cfb20eaa7dd33ad43237a9cb4058f6d5ec2f2697
satyam kumar <72137586+satyam-dot@users.noreply.github.com>
2020-10-21 15:36:26 +0530

    Update queue_using_array.cpp

--- Queue/queue_using_array.cpp
+++ Queue/queue_using_array.cpp
@@ -1,108 +1,144 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
-template <typename T>
 
-class QueueUsingArray {
-	T* data;
-	int firstIndex;
-	int nextIndex;
-	int size;
-	int capacity;
+class queue
+{
+private:
+    int front;
+    int rear;
+    int arr[3];
 public:
-	QueueUsingArray() {
-		data = new T[5];
-		firstIndex = -1;
-		nextIndex = 0;
-		size = 0;
-		capacity = 5;
-	}
-	int getsize() {
-		return size;
-	}
-	bool isEmpty() {
-		return size == 0;
-	}
-	void enqueue(T element) {
-		if (size == capacity)
-		{
-			int j = 0;
-			T* newData = new T[capacity * 2];
-			for (int i = firstIndex; i < capacity; i++) {
-				newData[j] = data[i];
-				j++;
-			}
-			for (int i = 0; i < firstIndex; i++) {
-				newData[j] = data[i];
-				j++;
-			}
-			delete [] data;
-			data = newData;
-			firstIndex = 0;
-			nextIndex = capacity;
-			capacity *= 2;
-		}
-		data[nextIndex] = element;
-		nextIndex = (nextIndex + 1) % capacity;
-		if (firstIndex == -1) {
-			firstIndex = 0;
-		}
-		size++;
-	}
-	T front() {
-		if (isEmpty()) {
-			cout << "Queue is empty!" << endl;
-			return 0;
-		}
-		return data[firstIndex];
-	}
-
-	T dequeue() {
-		if (isEmpty()) {
-			cout << "Queue is empty!" << endl;
-			return 0;
-		}
-		T ans = data[firstIndex];
-		firstIndex = (firstIndex + 1) % capacity;
-		size--;
-		if (size == 0) {
-			firstIndex = -1;
-			nextIndex = 0;
-		}
-		return ans;
-	}
-
+    queue()
+    {
+        front = rear = -1;
+        for (int i = 0; i < 3; i++)
+        {
+            arr[i] = 0;
+        }
+    }
+    bool isempty()
+    {
+        if (front == -1 && rear == -1)
+            return true;
+        else
+            return false;
+    }
+    bool isfull(){
+        if(rear == 2)
+        return true;
+        else
+        return false;
+    }
+    void enqueue(int val){
+        if (isfull())
+         {
+         cout<<"Queue is full"<<endl;
+         return ;
+         }
+         else if(isempty()){
+             rear = front = 0;
+             arr[rear] = val;
+         }
+        else{
+            rear ++;
+            arr[rear] = val;
+        }
+    }
+    int dequeue(){
+        int x;
+        if(isempty())
+        {
+        return 0;
+        
+        }
+        else if(rear == front){
+            x = arr[front];
+            arr[front] = 0;
+            front =rear = -1;
+            return x; 
+        }
+        else
+        {
+            int x = arr[front];
+            arr[front] = 0;
+            front++;
+            return x;
+        }
+    }
+    int count(){
+        return(rear-front+1);
+    }
+    void display(){
+        cout<<"all value of the queue are :"<<endl;
+        for (int i = 0; i < 3; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        
+    }
 };
 
+int main()
+{   
+    queue q1;
+    int value,option;
+    do
+    {
+        cout << "\nwhat operation do you want to perform ?select option number, enter 0 to exit " << endl;
 
-
-int main() {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-	QueueUsingArray<int> q;
-	q.enqueue(10);
-	q.enqueue(20);
-	q.enqueue(30);
-	q.enqueue(40);
-	q.enqueue(50);
-	q.enqueue(60);
-	q.enqueue(70);
-	q.enqueue(80);
-	q.enqueue(90);
-	cout << q.front() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.getsize() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.dequeue() << endl;
-	cout << q.front() << endl;
-	cout << q.isEmpty() << endl;
-
-
-	return 0;
+        cout << "1. enqueue()" << endl;
+        cout << "2. dequeue()" << endl;
+        cout << "3. isempty()" << endl;
+        cout << "4. isfull()" << endl;
+        cout << "5. count()" << endl;
+        cout << "6. display()" << endl;
+        cout << "7. clear screen()" << endl;
+        cin >> option;
+        switch (option)
+        {
+        case 0:
+            break;
+        case 1:
+            cout<<"\nenter the value of item to enqueue in the queue is: ";
+            cin>>value;
+            q1.enqueue(value);
+            break;
+        case 2:
+            if (q1.isempty())
+            {
+                cout << "queue is empty " << endl;
+            }
+            else
+            {
+                cout<<"dequeue operation is called,dequeu value is "<<q1.dequeue()<<endl;
+            }
+             break;
+        case 3:
+            if (q1.isempty())
+                cout << "queue is empty " << endl;
+            else
+                cout << "queue is full " << endl;
+            break;
+        case 4:
+            if (q1.isfull())
+                cout << " queue is full " << endl;
+            else
+                cout << " queue is empty " << endl;
+            break;
+        case 5:
+            cout<<"count function is called,count of item in queue is :"<<q1.count()<<endl;
+            break;
+        case 6:
+            cout<<"display function is called!!"<<endl;
+            q1.display();
+            break;
+        case 7:
+            system("cls");
+        default:
+            cout << "enter proper option number " << endl;
+            break;
+        }
+    } while (option != 0);
+    return 0;
 }
